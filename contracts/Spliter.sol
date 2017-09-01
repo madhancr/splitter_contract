d794271014b2b72280f9c221494855dd36d243b7
Madhan Ramalingam <madhan@madhans-mbp.attlocal.net>
2017-09-01 12:54:12 -0700

    made the following changes based on review comments from bob - made balances public - fixed withdraw() validation - simpified split method

--- contracts/Spliter.sol
+++ contracts/Spliter.sol
@@ -1,21 +1,12 @@
 pragma solidity ^0.4.6;
 
 contract Splitter {
-    address public owner;
-    mapping(address => uint) balances;
     
-
+    address public owner;
+    mapping(address => uint) public balances;
+    
     function Splitter() {
         owner = msg.sender;
-    }
-
-    function isAddress(address addr) 
-    public 
-    constant
-    returns (bool success) 
-    {
-        // simple check for now. TODO: add more checks
-        return addr != 0;
     }
 
     function split(address receiver1, address receiver2)
@@ -23,48 +14,28 @@ contract Splitter {
     payable
     returns(bool success) 
     {
-        if (receiver1 == receiver2) {revert();}
-        if (!isAddress(receiver1)) {revert();}
-        if (!isAddress(receiver2)) {revert();}
         if (msg.value == 0) {revert();}
 
         uint amtToSplit = msg.value;
-        
         // if odd amount is sent, refund remaining amt to sender
-        if (amtToSplit % 2 == 0) {
-            balances[receiver1] += amtToSplit/2;
-            balances[receiver2] += amtToSplit/2;
-        } else {
-            uint refundAmt = amtToSplit % 2;
-            amtToSplit = amtToSplit - refundAmt;
-            balances[receiver1] += amtToSplit/2;
-            balances[receiver2] += amtToSplit/2;
-            balances[msg.sender] += refundAmt;
-        }
-
+        uint refundAmt = amtToSplit % 2;
+        amtToSplit = amtToSplit - refundAmt;
+        balances[receiver1] += amtToSplit/2;
+        balances[receiver2] += amtToSplit/2;
+         // 0 will be refunded if amount is even. conditional check can be added to save gas
+        balances[msg.sender] += refundAmt;
         return true;
-    }
-
-    function getBalance(address target)
-    public
-    constant
-    returns(uint amount)
-    {
-        uint balance = 0;
-        if (isAddress(target)) {balance = balances[target];}
-        return balance;
     }
 
     function withdraw()
     public
     returns (bool success)
     {
-        address sender = msg.sender;
-        uint balance = getBalance(sender);
-        if (balance > 0) {
-            balances[sender] = 0;
-            sender.transfer(balance);
-        }
+        uint balance = balances[msg.sender];
+        if (balance == 0) {revert();}
+        
+        balances[msg.sender] = 0;
+        msg.sender.transfer(balance);
         return true;
     }
 
--- test/splitter.js
+++ test/splitter.js
@@ -32,29 +32,29 @@ contract('Splitter', function(accounts) {
       var splitAmt = (amountToSplit-refundAmt)/2;
       
 
-      return contract.getBalance(alice, {from:owner})
+      return contract.balances(alice, {from:owner})
         .then(_aliceBalance => {
           aliceBalance = _aliceBalance;
-          return contract.getBalance(bob, {from:owner})
+          return contract.balances(bob, {from:owner})
         })
         .then(_bobBalance => {
           bobBalance = _bobBalance;
-          return contract.getBalance(carol, {from:owner})
+          return contract.balances(carol, {from:owner})
         })
         .then(_carolBalance =>{
           carolBalance = _carolBalance;
           return contract.split(bob, carol, {from:alice, value:amountToSplit})
         })
         .then(function(){
-          return contract.getBalance(bob, {from:owner})
+          return contract.balances(bob, {from:owner})
         })
         .then(function(bobNewBalance){
           assert.equal((bobBalance.add(splitAmt)).toString(10), (bobNewBalance).toString(10), "bob's split is incorrect");
-          return contract.getBalance(carol, {from:owner})
+          return contract.balances(carol, {from:owner})
         })
         .then(function(carolNewBalance){
           assert.equal((carolBalance.add(splitAmt)).toString(10), (carolNewBalance).toString(10), "carols's split is incorrect");
-          return contract.getBalance(alice, {from:owner})
+          return contract.balances(alice, {from:owner})
         })       
         .then(function(aliceNewBalance){
           assert.equal( (aliceBalance.add(refundAmt)).toString(10), (aliceNewBalance).toString(10), "alice's refund amt is incorrect");
@@ -72,17 +72,17 @@ contract('Splitter', function(accounts) {
       .then(function(){
         // withdraw and check if balance is zero
         contract.withdraw({from:bob})
-        return contract.getBalance(bob, {from:owner})
+        return contract.balances(bob, {from:owner})
       })
       .then(function(bobNewBalance){
         assert.equal(zeroBalance.toString(10), (bobNewBalance).toString(10), "bob's balance is incorrect");
         contract.withdraw({from:carol})
-        return contract.getBalance(carol, {from:owner})
+        return contract.balances(carol, {from:owner})
       })
       .then(function(carolNewBalance){
         assert.equal(zeroBalance.toString(10), (carolNewBalance).toString(10), "carols's balance is incorrect");
         contract.withdraw({from:alice})
-        return contract.getBalance(alice, {from:owner})
+        return contract.balances(alice, {from:owner})
       })       
       .then(function(aliceNewBalance){
         assert.equal( zeroBalance.toString(10), (aliceNewBalance).toString(10), "alice's balance is incorrect");
